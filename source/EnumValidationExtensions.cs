4410b653c006b9cf8a8f5cb348a086fef5bc4d17
Jason Graham <jason.graham@csharplabs.com>
2017-01-31 20:20:33 -0800

    Initial commit

---
+++ source/EnumValidationExtensions.cs
@@ -0,0 +1,149 @@
+﻿//---------------------------------------------------------------------------- 
+//
+//  Copyright (C) CSharp Labs.  All rights reserved.
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+// 
+// History
+//  06/28/13    Created 
+//  12/23/16    Validated against negative values
+//---------------------------------------------------------------------------
+
+namespace System
+{
+    using System.ComponentModel;
+
+    public static class EnumValidationExtensions
+    {
+        /// <summary>
+        /// Throws an <see cref="InvalidEnumArgumentException"/> if the enumeration <see cref="value"/> is not defined or not a valid combination of flags.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <exception cref="InvalidEnumArgumentException">The <see cref="value"/> is invalid.</exception>
+        public static void ThrowIfInvalidEnumValue(this Enum value)
+        {
+            if (!value.IsDefined())
+                throw new InvalidEnumArgumentException($"The System.Enum Type: '{value.GetType()}' Value: '{value}' is not defined or does not contains a valid bit combination.");
+        }
+
+        /// <summary>
+        /// Determines if the enumeration value is defined or contains a valid combination of flags.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns>true if the value is defined or a valid combination of flags; otherwise, false.</returns>
+        public static bool IsDefined(this Enum value)
+        {
+            Type enumType = value.GetType();
+
+            if (value.HasFlags(enumType))
+                return value.AreFlagsDefined(enumType);
+            else
+                return Enum.IsDefined(enumType, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">An enumeration value or bit combination of flags specified in <paramref name="enumType"/>.</param>
+        /// <param name="enumType">An enumeration type.</param>
+        /// <returns>true if the enumeration can be treated as a set of flags; otherwise, false.</returns>
+        private static bool HasFlags(this Enum value, Type enumType)
+        {
+            return enumType.GetCustomAttributes(typeof(FlagsAttribute), false).Length > 0;
+        }
+
+        /// <summary>
+        /// Determines if the enumeration value contains a combination of valid flags.
+        /// </summary>
+        /// <param name="value">A bit combination of constant values.</param>
+        /// <param name="enumType">An enumeration type.</param>
+        /// <returns>true if all flags are defined in the enumeration; otherwise, false.</returns>
+        private static bool AreFlagsDefined(this Enum value, Type enumType)
+        {
+            //get the enumeration type code describing underlying type:
+            TypeCode code = value.GetTypeCode();
+
+            switch (code)
+            {
+                case TypeCode.Boolean:
+                case TypeCode.Char:
+                case TypeCode.Byte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64: //validate unsigned value
+                    {
+                        ulong flags = Convert.ToUInt64(value); //convert to 64-bit unsigned integer
+                        ulong flag;
+
+                        //iterate enumeration constants:
+                        foreach (object obj in Enum.GetValues(enumType))
+                        {
+                            flag = Convert.ToUInt64(obj); //get flag constants
+
+                            if (flag == flags) //last flag
+                                return true;
+
+                            //remove bits:
+                            flags &= ~flag;
+                        }
+                    }
+                    break;
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64: //validate signed value        
+                    {
+                        long flags = Convert.ToInt64(value); //convert to 64-bit signed integer
+
+                        if (flags < 0) //validate the flags are greater-than or equal to zero
+                            throw new ArgumentOutOfRangeException(nameof(value), $"The specified System.Enum value of '{flags}' and must be greater-than or equal to zero.");
+
+                        long flag;
+                        var enumValues = Enum.GetValues(enumType);
+
+                        //iterate enumeration constants:
+                        foreach (object obj in enumValues)
+                        {
+                            flag = Convert.ToInt64(obj); //get flag constants
+
+                            if (flag < 0) //validate the flag is greater-than or equal to zero
+                                throw new InvalidOperationException($"Cannot validate System.Enum '{enumType}', TypeCode: '{code}' with value {obj} that is less-than zero.");
+                        }
+
+                        //iterate enumeration constants:
+                        foreach (object obj in enumValues)
+                        {
+                            flag = Convert.ToInt64(obj); //get flag constants
+
+                            if (flag == flags) //last flag
+                                return true;
+
+                            //remove bits:
+                            flags &= ~flag;
+                        }
+                    }
+                    break;
+                default:
+                    throw new ArgumentException($"Cannot validate System.Enum '{enumType}' with TypeCode: '{code}'.", "value");
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/UnitTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace EnumValidationExtensionsTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [Flags]
+        private enum IntEnumFlags : int
+        {
+            Value0 = 0,
+            Value1 = 1,
+            Value2 = 2,
+            Value3 = 4
+        }
+
+        private enum IntEnum : int
+        {
+            Value0 = 0,
+            Value1 = 1,
+            Value2 = 2,
+            Value3 = 3
+        }
+
+        [Flags]
+        private enum InvalidIntEnumFlags : int
+        {
+            Value0 = 0,
+            Value1 = -1,
+            Value2 = -2,
+            Value3 = -4
+        }
+
+        [Flags]
+        private enum UIntEnumFlags : uint
+        {
+            Value0 = 0,
+            Value1 = 1,
+            Value2 = 2,
+            Value3 = 4
+        }
+
+        private enum UIntEnum : uint
+        {
+            Value0 = 0,
+            Value1 = 1,
+            Value2 = 2,
+            Value3 = 3
+        }
+
+        [TestMethod]
+        public void IntFlagTests()
+        {
+            Assert.IsTrue(((IntEnumFlags)0).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)1).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)2).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)3).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)4).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)5).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)6).IsDefined());
+            Assert.IsTrue(((IntEnumFlags)7).IsDefined());
+            Assert.IsFalse(((IntEnumFlags)8).IsDefined());
+
+            Assert.IsTrue(IntEnumFlags.Value0.IsDefined());
+            Assert.IsTrue(IntEnumFlags.Value1.IsDefined());
+            Assert.IsTrue(IntEnumFlags.Value2.IsDefined());
+            Assert.IsTrue(IntEnumFlags.Value3.IsDefined());
+            Assert.IsTrue((IntEnumFlags.Value1 | IntEnumFlags.Value2).IsDefined());
+            Assert.IsTrue((IntEnumFlags.Value1 | IntEnumFlags.Value2 | IntEnumFlags.Value3).IsDefined());
+        }
+
+        [TestMethod]
+        public void UIntFlagTests()
+        {
+            Assert.IsTrue(((UIntEnumFlags)0).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)1).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)2).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)3).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)4).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)5).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)6).IsDefined());
+            Assert.IsTrue(((UIntEnumFlags)7).IsDefined());
+            Assert.IsFalse(((UIntEnumFlags)8).IsDefined());
+
+            Assert.IsTrue(UIntEnumFlags.Value0.IsDefined());
+            Assert.IsTrue(UIntEnumFlags.Value1.IsDefined());
+            Assert.IsTrue(UIntEnumFlags.Value2.IsDefined());
+            Assert.IsTrue(UIntEnumFlags.Value3.IsDefined());
+            Assert.IsTrue((UIntEnumFlags.Value1 | UIntEnumFlags.Value2).IsDefined());
+            Assert.IsTrue((UIntEnumFlags.Value1 | UIntEnumFlags.Value2 | UIntEnumFlags.Value3).IsDefined());
+        }
+
+        [TestMethod]
+        public void IntEnumTests()
+        {
+            Assert.IsTrue(((IntEnum)0).IsDefined());
+            Assert.IsTrue(((IntEnum)1).IsDefined());
+            Assert.IsTrue(((IntEnum)2).IsDefined());
+            Assert.IsTrue(((IntEnum)3).IsDefined());
+            Assert.IsFalse(((IntEnum)4).IsDefined());
+
+            Assert.IsTrue(IntEnum.Value0.IsDefined());
+            Assert.IsTrue(IntEnum.Value1.IsDefined());
+            Assert.IsTrue(IntEnum.Value2.IsDefined());
+            Assert.IsTrue(IntEnum.Value3.IsDefined());
+        }
+
+        [TestMethod]
+        public void UIntEnumTests()
+        {
+            Assert.IsTrue(((UIntEnum)0).IsDefined());
+            Assert.IsTrue(((UIntEnum)1).IsDefined());
+            Assert.IsTrue(((UIntEnum)2).IsDefined());
+            Assert.IsTrue(((UIntEnum)3).IsDefined());
+            Assert.IsFalse(((UIntEnum)4).IsDefined());
+
+            Assert.IsTrue(UIntEnum.Value0.IsDefined());
+            Assert.IsTrue(UIntEnum.Value1.IsDefined());
+            Assert.IsTrue(UIntEnum.Value2.IsDefined());
+            Assert.IsTrue(UIntEnum.Value3.IsDefined());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidEnumArgumentException))]
+        public void InvalidEnumTest()
+        {
+            ((IntEnum)int.MaxValue).ThrowIfInvalidEnumValue();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void OutOfRangeEnumValueTest()
+        {
+            ((IntEnumFlags)int.MinValue).IsDefined();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidEnumValueDefinedTest()
+        {
+            InvalidIntEnumFlags.Value0.IsDefined();
+        }
+    }
+}
